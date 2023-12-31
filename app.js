fe96c83696aac406a711c11b1671e0e92bc5fbe8
Pranav Kamble <98037916+Pranav2302@users.noreply.github.com>
2024-01-01 02:58:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 
+ const Url="https://api.weatherapi.com/v1/current.json?key=844774d741694d3581983113232712&q=pune"
+
 const apikey= "844774d741694d3581983113232712"
-const body = document.querySelector("body");
-let text;
-const search=document.querySelector(".search-bar")
-const btn=document.querySelector("button")
-btn.addEventListener("click",textfun)
 
-document.querySelector(".search-bar").addEventListener("keydown",function(event){
-  if(event.key=="Enter"){
-    textfun()
-  }
-})
  
-function textfun(){
-   text = search.value;
-   weather.fetchweather(text);
-}
+const body = document.querySelector("body");
+// let text;
+let search = document.querySelector(".search-bar");
+const btn = document.querySelector("button")
+  
+const enter=document.querySelector(".search-bar")
+  
 
 
-let weather={
-  fetchweather:function(city){
+
+
+
+let weather =function (city){
     fetch(
       "https://api.weatherapi.com/v1/current.json?key=" + apikey + "&q=" + city
     )
@@ -33,7 +30,7 @@ let weather={
       })
       .then((data) => {
          try {
-          this.savedisplayelement(data);
+          savedisplayelement(data);
          } catch (error) {
            console.log(error);
          }
@@ -43,9 +40,10 @@ let weather={
         console.log("this", error);
       });
     
-  },
+  }
 
-    savedisplayelement:function(data){
+const savedisplayelement=function(data){
+  
       const name= data.location.name;
       const Humidity=data.current.humidity;
       const temp_c=data.current.temp_c;
@@ -54,26 +52,32 @@ let weather={
       const wind = data.current.wind_kph;
       const time=data.location.localtime;
       const imageback="https://source.unsplash.com/1600x900/?" + name;
-      console.log(name,Humidity,temp_c,icon,desc,wind,time)
+   
       
       body.setAttribute("background",imageback);
       document.querySelector(".city").innerText="Weather in "+name;
       document.querySelector(".temp").innerText = temp_c;
       document.querySelector(".icon").src=icon;
-      console.log(icon)
+   
       document.querySelector(".desc").innerText = desc;
       document.querySelector(".humidity").innerText ="Humidity:  "+Humidity+"%";
       document.querySelector(".wind").innerText = "Wind Speed:  "+wind+"Km/h";
       document.querySelector(".time").innerText = "Date And Time:  "+time;
-    
+        document.getElementById("hide").removeAttribute("hidden");
  
   }
  
-  
-} 
 
+btn.addEventListener("click", textfun(search.value));
+enter.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    textfun(search.value);
+  }
+});
 
+function textfun(text) {
+  document.querySelector(".background").src =
+    "https://source.unsplash.com/1600x900?nature";
 
-
-
-
+  weather(text);
+}
